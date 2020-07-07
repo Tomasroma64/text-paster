802f67f44295dee0c58e78b0960bfd269c726199
tomasmaillo <tomasroma64@gmail.com>
2020-07-07 13:27:00 +0200

    No enter button

--- background.js
+++ background.js
@@ -2,6 +2,6 @@ chrome.commands.onCommand.addListener(function(command) {
 
     if (command === "scroll") {
         chrome.tabs.executeScript({ file: 'content.js' });
-
     }
+
 });
--- popup.js
+++ popup.js
@@ -1,3 +1,13 @@
-document.getElementById('updateemoji').onclick = () => {
-    chrome.storage.local.set({ "text": document.getElementById("text-input").value });
-};
+$(document).ready(function() {
+
+    chrome.storage.local.get('text', function(result) {
+
+        $('#text-input').val(result.text);
+    });
+
+});
+
+
+$('#text-input').change(function() {
+    chrome.storage.local.set({ "text": $("#text-input").val() });
+});
